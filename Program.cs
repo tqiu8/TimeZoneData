417b4164d63beb10fb4d291eb12d78ee1d0a4242
Tammy Qiu <tammy.qiu@yahoo.com>
2020-07-20 16:12:13 -0400

    generalize to all data files

--- Program.cs
+++ Program.cs
@@ -4,59 +4,91 @@ using System.Collections.Generic;
 using System.IO;
 using System.Text;
 using System.Text.Json;
+using System.Linq;
 
 namespace Microsoft.AspNetCore.Components.WebAssembly.Runtime.TimeZone
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            var tzFolder = Path.Combine(Directory.GetCurrentDirectory(), "obj", "data", "output");
-            if (!Directory.Exists(tzFolder))
-            {
-                throw new DirectoryNotFoundException("TZOutput file does not exist. Use run.sh to run this project");
-            }
-
-            // https://en.wikipedia.org/wiki/Tz_database#Area
-            var areas = new[] { "Africa", "America", "Antarctica", "Arctic", "Asia", "Atlantic", "Australia", "Europe", "Indian", "Pacific" };
-            var stream = new MemoryStream();
+        
+        static (byte[] json_bytes, MemoryStream stream) enumerateData (string[] sub_folders, string output_folder) {
             var indices = new List<object[]>();
+            var stream = new MemoryStream();
 
-            foreach (var area in areas)
+            foreach (var folder in sub_folders)
             {
-                var directoryInfo = new DirectoryInfo(Path.Combine(tzFolder, area));
+                var directoryInfo = new DirectoryInfo(Path.Combine(output_folder, folder));
                 foreach (var entry in directoryInfo.EnumerateFiles("*", SearchOption.AllDirectories))
                 {
-                    
-                    var relativePath = entry.FullName.Substring(tzFolder.Length).Trim('/');
-                    System.Console.WriteLine(entry.FullName);
+                    var relativePath = entry.FullName.Substring(output_folder.Length).Trim('/');
+                    indices.Add(new object[] { relativePath, entry.Length});
 
-
-                    indices.Add(new object[] { relativePath, entry.Length });
-                    
                     using (var readStream = entry.OpenRead())
                         readStream.CopyTo(stream);
                 }
             }
-
+            
             stream.Position = 0;
             var options = new JsonSerializerOptions
-                {
-                    WriteIndented = false
-                };
+            {
+                WriteIndented = false
+            };
 
             var jsonBytes = JsonSerializer.SerializeToUtf8Bytes(indices);
-            using (var timezoneFile = File.OpenWrite("dotnet.timezones.blat"))
+            
+            return (jsonBytes, stream);
+        }
+
+        static (byte[] json_bytes, MemoryStream stream) readTimeZone (string folder) {
+            // https://en.wikipedia.org/wiki/Tz_database#Area
+            var areas = new[] { "Africa", "America", "Antarctica", "Arctic", "Asia", "Atlantic", "Australia", "Europe", "Indian", "Pacific" };
+
+            return enumerateData (areas, folder);
+        }
+
+        static (byte[] json_bytes, MemoryStream stream) readGeneralData (string input_folder) {
+            var DirectoryInfo = new DirectoryInfo (input_folder);
+            string[] sub_folders = DirectoryInfo.EnumerateFileSystemInfos().Select(f => f.Name).ToArray();
+
+            return enumerateData (sub_folders, input_folder);
+        }
+
+        static void Main(string[] args)
+        {
+            string type = args[0];
+            string input_dir = args[1];
+            string data_name = args[2];
+            (byte[] json_bytes, MemoryStream stream) data;
+            
+            var folder = Path.Combine(Directory.GetCurrentDirectory(), "obj", "data", "output");
+            if (!Directory.Exists(folder))
             {
+                throw new DirectoryNotFoundException("Output directory does not exist. Use run.sh to run this project");
+            }
+            
+            if (type == "timezone") {
+                data = readTimeZone (folder);
+            }
+            else {
+                data = readGeneralData (input_dir);
+            }
+            
+            using (var file = File.OpenWrite(data_name))
+            {
+                var jsonBytes = data.json_bytes;
+                var stream = data.stream;
                 var bytes = new byte[4];
                 var magicBytes = Encoding.ASCII.GetBytes("talb");
                 BinaryPrimitives.WriteInt32LittleEndian(bytes, jsonBytes.Length);
-                timezoneFile.Write(magicBytes);
-                timezoneFile.Write(bytes);
-                timezoneFile.Write(jsonBytes);
+                file.Write(magicBytes);
+                file.Write(bytes);
+                file.Write(jsonBytes);
                 
-                stream.CopyTo(timezoneFile);
+                stream.CopyTo(file);
             }
+            
+
+            
         }
     }
 }
--- run.sh
+++ run.sh
@@ -1,7 +1,34 @@
 #!/usr/bin/env bash
+set -e
+set -u
 
 input_dir="obj/data/input"
 output_dir="obj/data/output"
+data_name="dotnet.timezones.blat"
+type="timezone"
+
+while (("$#));
+do 
+    key="${1}"
+
+    case ${key} in
+    -i|--input_dir)
+        input_dir="$2"
+        shift 2
+        ;;
+    -t|--type)
+        type="$2"
+        shift 2
+        ;;
+    -d|-data_name)
+        data_name="$2"
+        shift 2
+        ;;
+    *)
+        shift
+        ;;
+    esac
+done
 
 if [[ -d "obj/data" ]]
 then
@@ -11,14 +38,16 @@ fi
 mkdir -p "$input_dir"
 mkdir "$output_dir"
 
-curl -L https://data.iana.org/time-zones/tzdata-latest.tar.gz -o "$input_dir/tzdata.tar.gz"
-tar xvzf "$input_dir/tzdata.tar.gz" -C "$input_dir"
+if [ type == "timezone" ]; then
+    curl -L https://data.iana.org/time-zones/tzdata-latest.tar.gz -o "$input_dir/tzdata.tar.gz"
+    tar xvzf "$input_dir/tzdata.tar.gz" -C "$input_dir"
 
-files=("africa"  "antarctica"  "asia"  "australasia"  "etcetera"  "europe"  "northamerica"  "southamerica" "backward")
+    files=("africa"  "antarctica"  "asia"  "australasia"  "etcetera"  "europe"  "northamerica"  "southamerica" "backward")
 
-for file in "${files[@]}"
-do
-    zic -d "$output_dir" "$input_dir/$file"
-done
+    for file in "${files[@]}"
+    do
+        zic -d "$output_dir" "$input_dir/$file"
+    done
+fi
 
-dotnet run
+dotnet run $type $input_dir $data_name
